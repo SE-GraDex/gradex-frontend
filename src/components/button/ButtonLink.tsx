cb06ae43c44f99d7d79235ac2d1e62379decc959
LuckyThanet <thanet.jomp@mail.kmutt.ac.th>
2024-11-06 21:06:10 +0700

    feat:navbar and button

---
+++ src/components/button/ButtonLink.tsx
@@ -0,0 +1,21 @@
+// components/ButtonLink.tsx
+import React from 'react';
+
+interface ButtonLinkProps {
+  label: string;
+  link: string;
+  className?: string; 
+}
+
+const ButtonLink: React.FC<ButtonLinkProps> = ({ label, link, className }) => {
+  return (
+    <a 
+      href={link} 
+      className={!className ? `w-[160px] h-[48px] border border-black rounded-full text-black hover:bg-black hover:text-white transition-all duration-200 flex justify-center items-center` : className}
+    >
+      {label}
+    </a>
+  );
+};
+
+export default ButtonLink;
