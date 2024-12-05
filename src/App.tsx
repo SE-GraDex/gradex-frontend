cf432912d8f79b872edd5abbbeb4b272234bdbab
LuckyThanet <thanet.jomp@mail.kmutt.ac.th>
2024-12-05 16:40:34 +0700

    fix:landing and model

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
-// import Home from './pages/LandingPage/Home';
 import Navbar from './components/Navbar'
-// import Footer from './components/Footer';
-// import Top from './pages/Top/Top';
 import Delivery from './pages/DeliveryCenter/DeliveryCenter'
 import Ongoing from './pages/DeliveryCenter/Ongoing/Ongoing'
 import Complete from './pages/DeliveryCenter/CompleteShip/CompleteShip'
@@ -12,7 +9,6 @@ import Register from './pages/LoginAndRegister/register'
 import MealPreparation from './pages/MealPrePage/MealPreparationPage'
 import Hungry from './pages/WhoHungry'
 import Recipe from './pages/Recipe/Recipe'
-import Sub from './pages/Subscribtion/Subscribtion'
 import History from './pages/ShippingPage/History'
 import Food from './pages/Recipe/RecipeFood'
 import IngredientManagement from './pages/MenuLab/IngredientManagment/IngredientManagementPage'
@@ -34,6 +30,7 @@ function Layout({ children }: { children: React.ReactNode }) {
     '/register',
     '/hungry',
     '/mealmanagementpage',
+    '/ingredientManagement',
   ].includes(location.pathname)
 
   return (
@@ -44,37 +41,48 @@ function Layout({ children }: { children: React.ReactNode }) {
   )
 }
 
+function AppContent() {
+  const location = useLocation()
+  const backgroundColors = [
+    { color: 'bg-[#C6FFEA]', path: ['/', '/register', '/login', '/hungry', '/recipe-book', '/shipping', '/recipe-food', '/shippingselectmonth', '/subscription', '/credit-card-payment', '/qr-code-payment', '/all-payment'] },
+    { color: 'bg-[#7BB3B5]', path: ['/delivery-center', '/ongoing', '/complete', '/menulab', '/ingredientmanagement' ,'/mealmanagementpage'] },
+  ]
+
+  const currentBgColor = backgroundColors.find(bg => bg.path.some(p => p === location.pathname))?.color || 'bg-white'
+
+  return (
+    <div className={`${currentBgColor} min-h-screen`}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/delivery-center" element={<Delivery />} />
+          <Route path="/ongoing" element={<Ongoing />} />
+          <Route path="/complete" element={<Complete />} />
+          <Route path="/menulab" element={<Menu />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/meal-preparation" element={<MealPreparation />} />
+          <Route path="/hungry" element={<Hungry />} />
+          <Route path="/recipe-book" element={<Recipe />} />
+          <Route path="/shipping" element={<History />} />
+          <Route path="/recipe-food" element={<Food />} />
+          <Route path="/ingredientmanagement" element={<IngredientManagement />} />
+          <Route path="/mealmanagementpage" element={<MealManagementPage />} />
+          <Route path="/shippingselectmonth" element={<ShippingSelectMonth />} />
+          <Route path="/subscription" element={<Subscription />} />
+          <Route path="/credit-card-payment" element={<CreditCardPaymentPage />} />
+          <Route path="/qr-code-payment" element={<QRCodePaymentPage />} />
+          <Route path="/all-payment" element={<AllPaymentPage />} />
+        </Routes>
+      </Layout>
+    </div>
+  )
+}
+
 export default function App() {
   return (
-    <div className="bg-[#C6FFEA] min-h-screen">
-      <Router>
-        <Layout>
-          <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/" element={<LandingPage />} />
-            {/* <Route path="/top" element={<Top />} /> */}
-            <Route path="/delivery-center" element={<Delivery />} />
-            <Route path="/ongoing" element={<Ongoing />} />
-            <Route path="/complete" element={<Complete />} />
-            <Route path="/menulab" element={<Menu />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/meal-preparation" element={<MealPreparation />} />
-            <Route path="/hungry" element={<Hungry />} />
-            <Route path="/recipe-book" element={<Recipe />} />
-            <Route path="/sub" element={<Sub />} />
-            <Route path="/shipping" element={<History />} />
-            <Route path="/recipe-food" element={<Food />} />
-            <Route path="/ingredientmanagement" element={<IngredientManagement />} />
-            <Route path="/mealmanagementpage" element={<MealManagementPage />} />
-            <Route path="/shippingselectmonth" element={<ShippingSelectMonth />} />
-            <Route path="/subscription" element={<Subscription />} />
-            <Route path="/credit-card-payment" element={<CreditCardPaymentPage />} />
-            <Route path="/qr-code-payment" element={<QRCodePaymentPage />} />
-            <Route path="/all-payment" element={<AllPaymentPage />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </div>
+    <Router>
+      <AppContent />
+    </Router>
   )
 }
--- src/components/ModalMeal.tsx
+++ src/components/ModalMeal.tsx
@@ -1,5 +1,8 @@
-import ButtonLink from './button/ButtonLink'
 import cross from '../assets/images/cross.svg'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 interface ModalProps {
   isOpen: boolean
@@ -11,9 +14,57 @@ interface ModalProps {
   logofood?: string
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, content, type, logo, food, logofood }) => {
-  if (!isOpen) return null
+interface IMenu {
+  menu_title: string
+  menu_description: string
+  ingredient_list: IIngredient[]
+  package: 'Basic' | 'Deluxe' | 'Premium'
+  menu_image: string
+}
 
+interface IIngredient {
+  name: string
+  portion: number
+  pricePerUnit: number
+  unit: string
+}
+
+
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, content, type, logo, food, logofood }) => {
+  if (!isOpen) return null;
+  const [selectMenu, setSelectMenu] = useState<IMenu | null>(null);
+  
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    const fetchTopMenus = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/dailyorderlist/getTopThreeOrders');
+        console.log(response.data);
+        const selectedMenu = response.data.menus.find((menu: any) => menu.menu_title === food);
+        if (selectedMenu) {
+          const temp: IMenu = {
+            menu_title: selectedMenu.menu_title,
+            menu_description: selectedMenu.menu_description,
+            ingredient_list: selectedMenu.ingredient_list,
+            package: selectedMenu.package,
+            menu_image: selectedMenu.menu_image,
+          };
+          setSelectMenu(temp);
+          // console.log(temp);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchTopMenus();
+  }, [food])
+
+  const handleClick = (item: IMenu) => {
+    console.log(item);
+    navigate('/recipe-food', { state: item })
+  }
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
       <div className="relative bg-white p-6 rounded-3xl shadow-lg w-[760px] h-[800px] flex flex-col">
@@ -38,11 +89,12 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, content, type, logo, foo
           <div className="text-topic text-center font-medium">{content}</div>
         </div>
         <div className="flex items-center justify-center mt-4">
-          <ButtonLink
-            label={'Recipe Book'}
-            link={''}
-            className={`w-[175px] h-[48px] mb-5 text-[16px] bg-[#30E06C] border rounded-full text-black hover:bg-white hover:text-[#066426] hover:border-[#30E06C] transition-all duration-200 flex items-center justify-center`}
-          />
+          <div
+            className={`w-[175px] h-[48px] mb-5 text-[16px] bg-[#30E06C] border rounded-full text-black hover:bg-white hover:text-[#066426] hover:border-[#30E06C] transition-all duration-200 flex items-center justify-center cursor-pointer`}
+            onClick={() => handleClick(selectMenu as IMenu)}
+          >
+            Recipe Book
+          </div>
         </div>
       </div>
     </div>
